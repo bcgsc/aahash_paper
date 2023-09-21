5e35739e53e6b33316840250c2ecde60902a405e
jowong04 <jowong@hpce704.hpc.bcgsc.ca>
2023-09-21 09:39:14 -0700

    use version 1.6.0 of btllib aahash code

--- src/btllib/aahash.cpp
+++ src/btllib/aahash.cpp
@@ -32,7 +32,7 @@ AAHash::init()
     pos = std::numeric_limits<std::size_t>::max();
     return false;
   }
-  const uint64_t hash_value = aahash_base(seq + pos, k, level);
+  uint64_t hash_value = aahash_base(seq + pos, k, level);
   nte64(hash_value, k, hash_num, hashes_array.get());
   initialized = true;
   return true;
@@ -45,14 +45,13 @@ AAHash::roll()
     return init();
   }
   if (pos >= seq_len - k) {
-    pos = std::numeric_limits<std::size_t>::max();
     return false;
   }
   if (AA_SEED_TABLE[(unsigned char)(seq[pos + k])] == AA_SEED__) {
     pos += k;
     return init();
   }
-  const uint64_t hash_value =
+  uint64_t hash_value =
     aahash_roll(hashes_array[0], k, seq[pos], seq[pos + k], level);
   nte64(hash_value, k, hash_num, hashes_array.get());
   ++pos;
