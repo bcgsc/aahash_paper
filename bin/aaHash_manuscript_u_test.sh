953a5f0e1875c2400a1250d9ee0caab34cda7665
jowong04 <jowong@hpcg02.hpc.bcgsc.ca>
2023-09-26 02:02:22 -0700

    removed test scripts

--- bin/aaHash_manuscript_u_test.sh
+++
@@ -1,8 +0,0 @@
-echo "##############################################"
-echo "uniformity simulated"
-echo "##############################################"
-./aaTest -j 48 -q 1000000 -l 250 -g 250 -t 1000000 -m 3 -a 5 -h 5 -u ./
-echo "##############################################"
-echo "uniformity real"
-echo "##############################################"
-./aaTest -j 48 -q 1000000 -l 250 -g 250 -t 1000000 -m 3 -a 5 -h 5 -u -i genes.fa uniprot-reviewed_yes+AND+proteome_up000005640.singleLine.fasta
